a82dfb1c22de54f531f7db02a744fde3c9cc4ebd
Julicoder10 <julian.andres.aquino33@gmail.com>
2023-04-22 21:01:38 -0300

    Mi portfolio, me falta mejorar el navbar y agregar mi mejor website a las demos (entre otros detalles minimos).

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll('section');
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('fade-in');
+      observer.unobserve(entry.target);
+    } else {
+      entry.target.classList.remove('fade-in');
+    }
+  });
+});
+
+sections.forEach(section => observer.observe(section));
