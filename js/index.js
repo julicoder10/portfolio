379a86770c217deb3870410be8e964a8f13e6e8b
Julian Aquino <julicoder10@gmail.com>
2023-05-07 15:02:14 -0300

    cambios en el diseño y funcionalidades

--- js/index.js
+++ js/index.js
@@ -12,3 +12,23 @@ const observer = new IntersectionObserver(function(entries, observer) {
 });
 
 sections.forEach(section => observer.observe(section));
+
+
+const numero = document.getElementById('numero');
+
+numero.addEventListener('click', () => {
+  // Crear un elemento de texto temporal para copiar el número
+  const tempInput = document.createElement('input');
+  tempInput.setAttribute('value', numero.innerText);
+  document.body.appendChild(tempInput);
+
+  // Seleccionar y copiar el número
+  tempInput.select();
+  document.execCommand('copy');
+
+  // Eliminar el elemento de texto temporal
+  document.body.removeChild(tempInput);
+
+  // Añadir un mensaje de confirmación
+  alert('El número ha sido copiado al portapapeles.');
+});
